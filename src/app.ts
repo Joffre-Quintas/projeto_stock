6cc0b6008e6fd1f9f647565fd8b6fc8157ffabe1
Joffre Quintas <joffrequintasdev@hotmail.com>
2023-10-18 13:18:52 -0300

    Prisma initialized

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
-import express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 export { app };
