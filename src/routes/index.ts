7e09258354d2feea5c5740fb84afbf1d56df5008
Gabriel Navarro <gabpnavarro@hotmail.com>
2023-10-29 21:08:55 -0300

    Criação do CRUD do controlador de empregados

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
 import AddressController from "../controllers/AddressController";
+import EmployeeController from "../controllers/EmployeeController";
 
 const route = Router();
 
 route.get("/allAddress", AddressController.findAllAdress);
+
+
+route.get("/allEmployee", EmployeeController.findAllEmployee);
+route.post("/createNewEmployee", EmployeeController.createNewEmployee);
+route.put("/updateEmployee", EmployeeController.updateEmployee);
+route.delete("/deleteEmployee", EmployeeController.deleteEmployee);
 
 export default route;
