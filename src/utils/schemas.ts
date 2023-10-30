4aa2e7e89ef4c1d5f158994af4efca01c8a9eca4
Gabriel Navarro <gabpnavarro@hotmail.com>
2023-10-30 15:31:25 -0300

    Aprimorando textos de respostas das validações

--- src/utils/schemas.ts
+++ src/utils/schemas.ts
@@ -30,20 +30,20 @@ class schema {
     })
 
     static employee = joi.object({
-        fullName: joi.string().required().messages({
-            'any.required': 'O campo fullName é obrigatório',
-            'string.empty': 'O campo fullName não pode ficar vazio', 
-            'string.base': 'O campo fullName deve ser escrito no formato de string',
+        fullName: joi.string().messages({
+            'any.required': 'O campo nome completo é obrigatório',
+            'string.empty': 'O campo nome completo não pode ficar vazio', 
+            'string.base': 'O campo nome completo deve ser escrito no formato de string',
         }),
 
-        hireDate: joi.string().regex(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/).required().messages({
-            'string.pattern.base': 'O campo hireDate está com o formato inválido, ele deve seguir o padrão ISO 8601: "YYYY-MM-DDTHH:MM:SSZ"',
-            'any.required': 'O campo hireDate é obrigatório',
-            'string.empty': 'O campo hireDate não pode ficar vazio', 
-            'string.base': 'O campo hireDate deve ser escrito no formato de string',
+        hireDate: joi.string().regex(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/).messages({
+            'string.pattern.base': 'O campo data de contratação está com o formato inválido, ele deve seguir o padrão ISO 8601: "YYYY-MM-DDTHH:MM:SSZ"',
+            'any.required': 'O campo data de contratação é obrigatório',
+            'string.empty': 'O campo data de contratação não pode ficar vazio', 
+            'string.base': 'O campo data de contratação deve ser escrito no formato de string',
         }),
 
-        office: joi.string().required().messages({
+        office: joi.string().messages({
             'any.required': 'O campo função de trabalho é obrigatório',
             'string.empty': 'O campo função de trabalho não pode ficar vazio',
             'string.base': 'O campo função de trabalho deve ser escrito no formato de string', 
