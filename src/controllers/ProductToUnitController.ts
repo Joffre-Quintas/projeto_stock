21c3d0834ae1e9defe5363b2908dfc38aaee7366
Gabriel Navarro <gabpnavarro@hotmail.com>
2023-11-02 23:04:16 -0300

    Controller relation product and unit

--- src/controllers/ProductToUnitController.ts
+++ src/controllers/ProductToUnitController.ts
@@ -10,29 +10,100 @@ class ProductToUnitController{
         const updateAt = new Date();
 
         try {
-            const productUnit: any = { productId, unitId, quantity, updateAt };
+            const existProductUnit = await prisma.productToUnit.findFirst({ where: { productId, unitId }});
 
-            await prisma.productToUnit.create({ 
-                data: productUnit,                   
-                include: { product: true, unit: true },
-            })
+            if(existProductUnit){
+                const id = existProductUnit.id;
 
-            res.status(201).json({ message: 'Relação criada com sucesso.' });
+                const quantityActual = existProductUnit.quantity + quantity;
+
+                await prisma.productToUnit.update({ where: {id}, data: {quantity: quantityActual, updateAt}})
+            
+                return res.status(200).json({ message: 'Relação produto e unidade já existe, quantidade atualizada com sucesso.' });
+            } else {
+                const productUnit: any = { productId, unitId, quantity, updateAt };
+    
+                await prisma.productToUnit.create({ 
+                    data: productUnit,                   
+                    include: { product: true, unit: true },
+                })
+    
+                return res.status(201).json({ message: 'Relação produto e unidade criada com sucesso.' });
+            }
+
         } catch (error) {
             res.status(500).json({ message: "Erro no servidor.", error });
         }
     }
 
     static findProductUnit = async (req:Request, res:Response) => {
-        try {
-            const productUnit = await prisma.productToUnit.findMany();
+        const id = Number(req.params.id);
 
-            res.status(201).json(productUnit);
+        try {
+        const productUnit = await prisma.productToUnit.findMany({ include: { product: true, unit: true } });
+
+        if(id){
+            const findProductUnit = productUnit.find( item => item.id === id);
+
+            if(findProductUnit){
+                return res.status(200).json(findProductUnit);
+            }else{
+                return res.status(200).json({ mensagem: "Relação do produto e unidade não encontrado"});
+            }
+        } else {
+            return res.status(200).json(productUnit);
+        }
+
         } catch (error) {
             res.status(500).json({ message: "Erro no servidor." });
-
         }
     } 
+
+    static updateProductUnit = async (req: Request, res: Response) => {
+        const { productId, unitId, quantity } = req.body;
+
+        const id = Number(req.params.id);
+
+        const updateAt = new Date();
+
+        try {
+            if(productId && unitId){
+                const existProductUnitEqual = await prisma.productToUnit.findFirst({ where: { productId, unitId }});
+    
+                if(existProductUnitEqual){
+                    const id = existProductUnitEqual.id;
+    
+                    await prisma.productToUnit.update({ where: {id}, data: {quantity, updateAt}})
+                
+                    return res.status(200).json({ message: 'Relação produto e unidade já existe, quantidade atualizada com sucesso.' });
+                }
+            }
+            
+            await prisma.productToUnit.update({ where: { id }, data: { productId, unitId, quantity, updateAt}})
+                
+            return res.status(200).json({ message: 'Relação do produto e unidade atualizada com sucesso.' });
+         
+        } catch (error) {
+            return res.status(500).json({ message: "Erro no servidor.", error });
+        }
+    }
+
+    static deleteProductUnit = async (req: Request, res: Response) => {
+        const id = req.params.id;
+
+        try {
+            await prisma.productToUnit.delete({
+                where: {
+                    id: Number(id)
+                }
+            })
+
+            return res.status(200).json({ message: "Relação do produto e unidade excluído com sucesso." });
+
+        } catch (error) {
+            res.status(500).json({ message: "Erro interno no servidor." });
+        }
+    }
 }
 
 export default ProductToUnitController;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -21,14 +21,16 @@ route.put("/employee", validation.requestBody(schema.employeeUpdate), validation
 route.delete("/employee", validation.requestBody(schema.id), validation.existEmployee, EmployeeController.deleteEmployee);
 
 route.post("/unit", validationFields, UnitController.newUnit)
-route.get("/unit/:id?", UnitController.findUnit)
+route.get("/unit/:id?", validation.existUnit, UnitController.findUnit)
 
 route.get("/product/:id?", ProductController.findAllProduct);
 route.post("/product", validation.requestBody(schema.product), validation.existProduct, ProductController.createNewProduct);
 route.put("/product", validation.requestBody(schema.productUpdate), validation.update , validation.existProduct, ProductController.updateProduct)
 route.delete("/product", validation.requestBody(schema.id), validation.existProduct, ProductController.deleteProduct)
 
-route.get("/productUnit", ProductToUnitController.findProductUnit)
-route.post("/productUnit", ProductToUnitController.createNewRelationProductUnit)
+route.get("/productUnit/:id?", ProductToUnitController.findProductUnit)
+route.post("/productUnit", validation.requestBody(schema.productUnit), validation.existProduct, validation.existUnit, ProductToUnitController.createNewRelationProductUnit)
+route.put("/productUnit/:id", validation.requestBody(schema.productUnitUpdate), validation.update, validation.productUnit, ProductToUnitController.updateProductUnit)
+route.delete("/productUnit/:id", validation.existProductUnit, ProductToUnitController.deleteProductUnit)
 
 export default route;
